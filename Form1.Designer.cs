549e2c95110a0e36654124ece8ddb80096469301
DangNguyenTH4 <dangnt520@wru.vn>
2018-11-14 09:56:31 +0700

    Get data with cookie

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.btnGetData = new System.Windows.Forms.Button();
+            this.btnGetDataCookie = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnGetData
@@ -41,11 +42,22 @@
             this.btnGetData.UseVisualStyleBackColor = true;
             this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
             // 
+            // btnGetDataCookie
+            // 
+            this.btnGetDataCookie.Location = new System.Drawing.Point(93, 12);
+            this.btnGetDataCookie.Name = "btnGetDataCookie";
+            this.btnGetDataCookie.Size = new System.Drawing.Size(131, 23);
+            this.btnGetDataCookie.TabIndex = 1;
+            this.btnGetDataCookie.Text = "GetData_Cookie\r\n";
+            this.btnGetDataCookie.UseVisualStyleBackColor = true;
+            this.btnGetDataCookie.Click += new System.EventHandler(this.btnGetDataCookie_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGetDataCookie);
             this.Controls.Add(this.btnGetData);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button btnGetData;
+        private System.Windows.Forms.Button btnGetDataCookie;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -31,12 +31,47 @@ namespace HTTP_Request_GetHowKteam
              * WebRequest
              * HttpRequest
              */
-            HttpRequest http = new HttpRequest();
-            string html = http.Get("https://www.howkteam.vn/").ToString();
-            //MessageBox.Show(html);
+            string url = "https://www.howkteam.vn";
+            var html = getData(url);
+            testData(html);
+        }
+        void testData(string html)
+        {
             File.WriteAllText("res.html", html);
             Process.Start("res.html");
-
+        }
+        string getData(string url,string cookie=null)
+        {
+            HttpRequest http = new HttpRequest();
+            http.Cookies = new CookieDictionary();
+            if (!string.IsNullOrEmpty(cookie))
+            {
+                AddCookie(http, cookie);
+            }
+            //http.UserAgent = "";
+            string html = http.Get(url).ToString();
+            return html;
+        }
+        void AddCookie(HttpRequest http,string cookie)
+        {
+            var temp = cookie.Split(';');
+            foreach(var item in temp)
+            {
+                var temp2 = item.Split('=');
+                if (temp2.Count() > 1)
+                {
+                    http.Cookies.Add(temp2[0], temp2[1]);
+                }
+            }
+        }
+       
+        private void btnGetDataCookie_Click(object sender, EventArgs e)
+        {
+            string url = "https://www.howkteam.vn/";
+            string cookie = "_ga=GA1.2.319522369.1539615594; _gid=GA1.2.1524716106.1542072362; ASP.NET_SessionId=znsibm2tpo0yougmclj1xcij; __RequestVerificationToken=rjZr3zvMR1Ov0lm6mw8SRgvBUDwJjqYguPlkd8IO_LdxVK_4qAPoaAGPEJytXu6WdvB3OzCsrDJhi_pKROgnMQRNgQ2qXQecGi6mhBheBHQ1; _gat=1; .AspNet.ApplicationCookie=kYhU6XznL0KQY9DPNIYNHZtCVUQ-Q-1-xfQQ_1UQ0osDD6UjXZE71axp2EmsqIbjvbz2-oIBhdP7KKxAPm0irCfiXSy9qCXMkHvslxJwthQIJuUAcWbXkBYb9Y3hSm8exyyJLviC0ck23vcsAdlMoYWqLvoSYla2Cw46dCLAwsBbv6JtBk_rCPOebFNYJWs15EQK2pC0WrlSfEwp1cIGTNu-OjyPVBj0QTzxzzCJ0u0y2jJsIEUL0_tqq8UPSEer10bXvViexQ8abwkHekhvf4Wh7KPen4yZjLO5B9JxkDUy_KXuUzeQiMvY_33n1VawsiuIPdtwQ59OFATa9e1ySd8s-bUKceSoAffRi1duPpxYEcLiOKkDm3KKJHBql3tGYpgj2vACyna9rBvgwvYJ30jm_0uJZDQXiEKZS40TK0hC4THjcYs__KQ4xzRK-LKIfzi5zggLh4fn_GVtgYMbuGEcL6ufP5Y10Qw8wWoeIREpMm08fvDIdJQF83dlU_nERY6NjMhTPTBqH-t6F8Ctxw";
+            var html = getData(url,cookie);
+            testData(html);
         }
     }
+
 }
